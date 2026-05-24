8653e18c2f9c62a23d2e526a67f02a8d98eb1602
Ziad_Mostafa <ziadmostafau@gmail.com>
2026-05-24 15:58:47 +0300

    Added player movement and wall placement and undo/redo moves

--- Game_state.py
+++ Game_state.py
@@ -2,10 +2,86 @@ class GameState:
     def __init__(self):
         self.vertical_walls = set()
         self.horizontal_walls = set()
-        self.first_player_pos = [0, 4]
-        self.second_player_pos = [8, 4]
+        self.first_player_pos = [4, 0]
+        self.second_player_pos = [4, 8]
         self.first_player_walls = 10
         self.second_player_walls = 10
         self.first_player_turn = True
         self.move_log = []
+        self.redo_moves = []
         self.winner = ""
+
+    def move(self, pos):
+        if self.first_player_turn:
+            self.move_log.append(["move", self.first_player_pos])
+            self.first_player_pos = pos
+        else:
+            self.move_log.append(["move", self.second_player_pos])
+            self.second_player_pos = pos
+        self.first_player_turn = not self.first_player_turn
+        self.redo_moves.clear()
+
+    def place_wall(self, pos, orientation):
+        if self.first_player_turn:
+            self.first_player_walls -= 1
+        else:
+            self.second_player_walls -= 1
+        if orientation == "horizontal_wall":
+            self.horizontal_walls.add(pos)
+            self.move_log.append(["horizontal", pos])
+        elif orientation == "vertical_wall":
+            self.vertical_walls.add(pos)
+            self.move_log.append(["vertical", pos])
+        self.first_player_turn = not self.first_player_turn
+
+    def undo(self):
+        if not self.move_log:
+            return
+        self.first_player_turn = not self.first_player_turn
+        last_move = self.move_log[-1]
+        move_type = last_move[0]
+        pos = last_move[1]
+        if move_type == "move":
+            if self.first_player_turn:
+                self.redo_moves.append([move_type, self.first_player_pos])
+                self.first_player_pos = pos
+            else:
+                self.redo_moves.append([move_type, self.second_player_pos])
+                self.second_player_pos = pos
+        elif move_type in ["horizontal", "vertical"]:
+            if self.first_player_turn:
+                self.first_player_walls += 1
+            else:
+                self.second_player_walls += 1
+            if move_type == "horizontal":
+                self.horizontal_walls.remove(pos)
+            elif move_type == "vertical":
+                self.vertical_walls.remove(pos)
+            self.redo_moves.append([move_type, pos])
+        self.move_log.pop()
+
+    def redo(self):
+        if not self.redo_moves:
+            return
+        last_move = self.redo_moves[-1]
+        move_type = last_move[0]
+        pos = last_move[1]
+        if move_type == "move":
+            if self.first_player_turn:
+                self.move_log.append(["move", self.first_player_pos])
+                self.first_player_pos = pos
+            else:
+                self.move_log.append(["move", self.second_player_pos])
+                self.second_player_pos = pos
+        elif move_type in ["horizontal", "vertical"]:
+            if self.first_player_turn:
+                self.first_player_walls -= 1
+            else:
+                self.second_player_walls -= 1
+            if move_type == "horizontal":
+                self.horizontal_walls.add(pos)
+            elif move_type == "vertical":
+                self.vertical_walls.add(pos)
+            self.move_log.append([move_type, pos])
+        self.first_player_turn = not self.first_player_turn
+        self.redo_moves.pop()
--- main.py
+++ main.py
@@ -13,8 +13,7 @@ class Game:
         self.running = True
         self.gs = GameState()
         self.font = pygame.font.Font('freesansbold.ttf', 30)
-        self.place_horizontal_walls = pygame.Rect((SQUARE_SIZE + WALL_THICKNESS) * 9, 350, 300, 50)
-        self.place_vertical_walls = pygame.Rect((SQUARE_SIZE + WALL_THICKNESS) * 9, 450, 300, 50)
+        self.mode = "neutral"
 
     def draw_game_state(self, game_state):
         for row in range(9):
@@ -22,11 +21,11 @@ class Game:
                 pygame.draw.rect(self.display_surface, FG_COLOR, ((SQUARE_SIZE + WALL_THICKNESS) * col,
                                                                   (SQUARE_SIZE + WALL_THICKNESS) * row,
                                                                   SQUARE_SIZE, SQUARE_SIZE))
-                if ([row, col] == game_state.first_player_pos):
+                if [col, row] == game_state.first_player_pos:
                     x_pos = (SQUARE_SIZE + WALL_THICKNESS) * col + SQUARE_SIZE / 2
                     y_pos = (SQUARE_SIZE + WALL_THICKNESS) * row + SQUARE_SIZE / 2
                     pygame.draw.circle(self.display_surface, FP_COLOR, (x_pos, y_pos), SQUARE_SIZE / 2)
-                elif ([row, col] == game_state.second_player_pos):
+                elif [col, row] == game_state.second_player_pos:
                     x_pos = (SQUARE_SIZE + WALL_THICKNESS) * col + SQUARE_SIZE / 2
                     y_pos = (SQUARE_SIZE + WALL_THICKNESS) * row + SQUARE_SIZE / 2
                     pygame.draw.circle(self.display_surface, SP_COLOR, (x_pos, y_pos), SQUARE_SIZE / 2)
@@ -61,13 +60,33 @@ class Game:
             winner_text = ""
         self.display_surface.blit(
             self.font.render(winner_text, True, FONT_COLOR),
-            ((SQUARE_SIZE + WALL_THICKNESS) * 9, 300))
-        pygame.draw.rect(self.display_surface, FG_COLOR, self.place_horizontal_walls)
+            ((SQUARE_SIZE + WALL_THICKNESS) * 9, 200))
+        pygame.draw.rect(self.display_surface, FG_COLOR, ((SQUARE_SIZE + WALL_THICKNESS) * 9, 300, 300, 50))
         self.display_surface.blit(self.font.render("Place horizontal wall", True, FONT_COLOR),
-                                  ((SQUARE_SIZE + WALL_THICKNESS) * 9, 362))
-        pygame.draw.rect(self.display_surface, FG_COLOR, self.place_vertical_walls)
+                                  ((SQUARE_SIZE + WALL_THICKNESS) * 9, 312))
+        pygame.draw.rect(self.display_surface, FG_COLOR, ((SQUARE_SIZE + WALL_THICKNESS) * 9, 400, 300, 50))
         self.display_surface.blit(self.font.render("Place vertical wall", True, FONT_COLOR),
-                                  ((SQUARE_SIZE + WALL_THICKNESS) * 9, 462))
+                                  ((SQUARE_SIZE + WALL_THICKNESS) * 9, 412))
+        if self.mode == "vertical_wall":
+            x_position, y_position = pygame.mouse.get_pos()
+            x_position = x_position // (SQUARE_SIZE + WALL_THICKNESS)
+            x_position = 7 if x_position > 7 else x_position
+            x_position = x_position * (SQUARE_SIZE + WALL_THICKNESS) + SQUARE_SIZE
+            y_position = y_position // (SQUARE_SIZE + WALL_THICKNESS)
+            y_position = 7 if y_position > 7 else y_position
+            y_position *= (SQUARE_SIZE + WALL_THICKNESS)
+            pygame.draw.rect(self.display_surface, WALL_HIGHLIGHT,
+                             (x_position, y_position, WALL_THICKNESS, 2 * SQUARE_SIZE + WALL_THICKNESS))
+        elif self.mode == "horizontal_wall":
+            x_position, y_position = pygame.mouse.get_pos()
+            x_position = x_position // (SQUARE_SIZE + WALL_THICKNESS)
+            x_position = 7 if x_position > 7 else x_position
+            x_position *= (SQUARE_SIZE + WALL_THICKNESS)
+            y_position = y_position // (SQUARE_SIZE + WALL_THICKNESS)
+            y_position = 7 if y_position > 7 else y_position
+            y_position = y_position * (SQUARE_SIZE + WALL_THICKNESS) + SQUARE_SIZE
+            pygame.draw.rect(self.display_surface, WALL_HIGHLIGHT,
+                             (x_position, y_position, 2 * SQUARE_SIZE + WALL_THICKNESS, WALL_THICKNESS))
 
     def run(self):
         while self.running:
@@ -76,6 +95,38 @@ class Game:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
+                elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                    x_coord = event.pos[0] // (SQUARE_SIZE + WALL_THICKNESS)
+                    y_coord = event.pos[1] // (SQUARE_SIZE + WALL_THICKNESS)
+                    if x_coord < 9 and y_coord < 9 and self.mode == "neutral":
+                        self.gs.move([x_coord, y_coord])
+                    elif 9 <= x_coord < 14 and y_coord == 5:
+                        if self.mode == "horizontal_wall":
+                            self.mode = "neutral"
+                        elif (self.gs.first_player_turn and self.gs.first_player_walls > 0) or (
+                                (not self.gs.first_player_turn) and self.gs.second_player_walls > 0):
+                            self.mode = "horizontal_wall"
+                    elif 9 <= x_coord < 14 and y_coord == 7:
+                        if self.mode == "vertical_wall":
+                            self.mode = "neutral"
+                        elif (self.gs.first_player_turn and self.gs.first_player_walls > 0) or (
+                                (not self.gs.first_player_turn) and self.gs.second_player_walls > 0):
+                            self.mode = "vertical_wall"
+                    elif self.mode in ["horizontal_wall", "vertical_wall"]:
+                        x_coord = 7 if x_coord > 7 else x_coord
+                        y_coord = 7 if y_coord > 7 else y_coord
+                        self.gs.place_wall((x_coord, y_coord), self.mode)
+                        self.mode = "neutral"
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_u:
+                        self.gs.undo()
+                        self.mode = "neutral"
+                    elif event.key == pygame.K_r:
+                        self.gs.redo()
+                        self.mode = "neutral"
+                    elif event.key == pygame.K_q:
+                        self.gs.__init__()
+                        self.mode = "neutral"
 
             # draw
             self.display_surface.fill(BG_COLOR)
